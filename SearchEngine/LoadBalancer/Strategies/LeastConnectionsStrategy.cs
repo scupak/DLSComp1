4e676839aafc2d7b74bd731655c0bc373f156a5e
scupak <antonibujko@hotmail.com>
2023-03-15 19:36:27 +0100

    Create LeastConnectionsStrategy.cs

---
+++ SearchEngine/LoadBalancer/Strategies/LeastConnectionsStrategy.cs
@@ -0,0 +1,13 @@
+﻿using LoadBalancer.LoadBalancer;
+using LoadBalancer.Models;
+
+namespace LoadBalancer.Strategies;
+
+public class LeastConnectionsStrategy : ILoadBalancerStrategy
+{
+    public string NextService(List<Service> services)
+    {
+        var service = services.OrderBy(s => s.NumberOfConnections).First();
+        return service.HostName;
+    }
+}
